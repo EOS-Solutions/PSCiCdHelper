6864b908c5ff574054f1414e01f42858b5f9ea6c
The Dumb <info@hemisphera.com>
2024-05-22 18:56:53 +0200

    Fix: remove debug code

--- install.ps1
+++ install.ps1
@@ -11,8 +11,6 @@ if (-not $RequiresDownload) {
     $RequiresDownload = -not [IO.File]::Exists($ModulePath)
 }
 
-$RequiresDownload = $true # debug
-
 if ($RequiresDownload) {
     $Filename = "$([IO.Path]::GetTempFileName()).zip"
     $TempFolder = [IO.Path]::Combine([IO.Path]::GetTempPath(), "PsCiCdHelper")
